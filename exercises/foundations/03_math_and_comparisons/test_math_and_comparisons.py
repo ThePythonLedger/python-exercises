8049bb795ee1c78dcda9355336634a9e299b9d31
razorblade23 <81590614+razorblade23@users.noreply.github.com>
2026-08-21 20:17:00 +0200

    Fix/wrong file being run as a test

--- exercises/foundations/03_math_and_comparisons/test_math_and_comparisons.py
+++ exercises/foundations/03_math_and_comparisons/test_math_and_comparisons.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_basic_math(run_script, expect_variables):
-    _, namespace = run_script("math_and_comparisons_solution.py")
+    _, namespace = run_script("math_and_comparisons.py")
     expect_variables(
         namespace,
         {"addition": 15, "substraction": -5, "multiplication": 50, "division": 0.5},
@@ -11,20 +11,20 @@ def test_basic_math(run_script, expect_variables):
 
 @pytest.mark.skip(reason="Finish earlier task first")
 def test_string_concat(run_script, expect_variables):
-    _, namespace = run_script("math_and_comparisons_solution.py")
+    _, namespace = run_script("math_and_comparisons.py")
     expect_variables(namespace, {"concat_name": "JaneDoe"})
 
 
 @pytest.mark.skip(reason="Finish earlier task first")
 def test_string_indexing(run_script, expect_variables):
-    _, namespace = run_script("math_and_comparisons_solution.py")
+    _, namespace = run_script("math_and_comparisons.py")
     expect_variables(namespace, {"first_letter": "M", "second_last_letter": "o"})
 
 
 @pytest.mark.skip(reason="Finish earlier task first")
 def test_convert_case(run_script, expect_variables):
     TEST_STRING = "HeLLo AND WelcomE"
-    _, namespace = run_script("math_and_comparisons_solution.py")
+    _, namespace = run_script("math_and_comparisons.py")
     expect_variables(
         namespace,
         {
